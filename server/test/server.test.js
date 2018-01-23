52ac7048552c9123525bbe4c42731ef8c23804d9
OR <oreyesc@gmail.com>
2018-01-23 12:25:03 -0500

    Add test for GET /todos

--- server/test/server.test.js
+++ server/test/server.test.js
@@ -5,11 +5,13 @@ const request   = require('supertest');
 const { app }   = require('./../server');
 const { Todo }  = require('./../models/todo');
 
+const todos     = [{ text: 'First test todo'}, { text: 'Second test todo'}];
+
 beforeEach((done) => {  // Delete everything form DB
 	Todo.remove({})
 		.then(() => {
-			done();
-		});
+			return Todo.insertMany(todos);
+		}).then(() => done());
 });
 
 describe('POST /todos',
@@ -30,7 +32,7 @@ describe('POST /todos',
 								return done(err);
 							}
 							
-							Todo.find()                         // DB data validation
+							Todo.find({ text })                         // DB data validation
 								.then((todos) => {
 									expect(todos.length).toBe(1);
 									expect(todos[0].text).toBe(text);
@@ -52,9 +54,22 @@ describe('POST /todos',
 							
 							Todo.find()
 								.then((todos) => {
-									expect(todos.length).toBe(0);
+									expect(todos.length).toBe(2);
 									done();
 								}).catch((error) => done(error))
 						});
 				});
 		});
+
+describe('GET /todos',
+	() => {
+		it('should get all todos', (done) => {
+			request(app)
+				.get('/todos')
+				.expect(200)
+				.expect((res) => {
+					expect(res.body.todos.length).toBe(2);
+				})
+				.end(done);
+		});
+	});
