ec5bea98be4b7025a8c5e214f342286583b2f902
OR <oreyesc@gmail.com>
2018-01-28 13:35:03 -0500

    Add GET /todos/:id

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 const express       = require('express');
 const bodyParser    = require('body-parser');
+const { ObjectID }  = require('mongodb');
 
 const { mongoose }  = require('./db/mongoose');
 const { Todo }      = require('./models/todo');
@@ -37,32 +38,57 @@ app.get('/todos',
 						});
 		});
 
-app.post('/users',
-	(req, res) => {
-		//console.log(`request: ${ JSON.stringify(req.body) }`);
-		let user    = new User({
-			name: req.body.name,
-			email: req.body.email
+// GET /todos/123456
+app.get('/todos/:id',
+		(req, res) => {
+			let id  = req.params.id;
+			
+			if (!ObjectID.isValid(id)) {    // validating id using valid id
+				return res.status(400).send('Id not valid');  // 404 - send back empty send
+			}
+			
+			Todo.findById(id)   // find ById
+				.then(  (todo) => { // success
+									if (!todo) { // if !todo - send back 404 with empty body
+										return res.status(404).send('Id has not been found');
+									}
+									
+									res.send({ todo }); // if todo - send it back
+								},
+						(error) => { // error
+										res.status(400).send('Id not found');  // 404 - send back empty send
+									});
+				// error
+					// 400 - and send empty body back
 		});
-		user.save()
-			.then((doc) => {
-					res.send(doc);
-				},
-				(err) => {
-					res.status(400).send(err);
-				});
-	});
+
+app.post('/users',
+		(req, res) => {
+			//console.log(`request: ${ JSON.stringify(req.body) }`);
+			let user    = new User({
+				name: req.body.name,
+				email: req.body.email
+			});
+			user.save()
+				.then((doc) => {
+						res.send(doc);
+					},
+					(err) => {
+						res.status(400).send(err);
+					});
+		});
 
 app.get('/users',
-	(req, res) => {
-		User.find()
-			.then(  (users) => {
-					res.send({ users });
-				},
-				(error) => {
-					res.status(400).send(error);
-				});
-	});
+		(req, res) => {
+			User.find()
+				.then(  (users) => {
+						res.send({ users });
+					},
+					(error) => {
+						res.status(400).send(error);
+					});
+		});
+
 
 app.listen(7000,
 			() => {
