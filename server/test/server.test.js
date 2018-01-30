0ff8a94a9af67c5bbc99027f9fed55da0cc0ad15
OR <oreyesc@gmail.com>
2018-01-30 08:25:12 -0500

    Add tests for DELETE /users/me/token

--- server/test/server.test.js
+++ server/test/server.test.js
@@ -336,3 +336,27 @@ describe('POST /users/login',
 						});
 				});
 		});
+
+
+describe('DELETE /users/me/token',
+		() => {
+			it('should remove auth token on logout',
+				(done) => {
+					request(app)
+						.delete('/users/me/token')  // DELETE request to /users/me/token
+						.set('x-auth',
+							users[0].tokens[0].token)  // Set x-auth equal to token
+						.expect(200)    // 200
+						.end((err, res) => {    // Find user, verify that tokens array has length of zero
+							if (err) {
+								return done(err);
+							}
+							
+							User.findById(users[0]._id)
+								.then((user) => {
+									expect(user.tokens.length).toBe(0);
+									done();
+								}).catch((e) => done(e));
+						});
+			});
+		});
