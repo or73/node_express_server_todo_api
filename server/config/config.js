3bf76e4917159cff6fd8f99bd98279b6573bac44
OR <oreyesc@gmail.com>
2018-01-30 10:34:25 -0500

    Update config to use JSON file

--- server/config/config.js
+++ server/config/config.js
@@ -1,11 +1,12 @@
 const env   = process.env.NODE_ENV || 'development';
 console.log(`env ***** ${ env }`);
 
-// configure app environment
-if (env === 'development') {
-	process.env.PORT    = 7000;
-	process.env.MONGODB_URI  = 'mongodb://localhost:27017/TodoApp';
-} else if (env === 'test') {
-	process.env.PORT    = 7000;
-	process.env.MONGODB_URI = 'mongodb://localhost:27017/TodoAppTest';
+
+if (env === 'development' || env === 'test') {
+	let config      = require('./config.json');
+	let envConfig   = config[env];  // development/test
+	
+	Object.keys(envConfig).forEach((key) => {
+		process.env[key]    = envConfig[key];
+	});
 }
--- server/test/seed/seed.js
+++ server/test/seed/seed.js
@@ -15,7 +15,7 @@ const users         = [{    // user with authentication token
 							password: 'userOnePass',
 							tokens: [{
 								access: 'auth',
-								token: jwt.sign({ _id: userOneId, access: 'auth' }, 'abc123').toString()
+								token: jwt.sign({ _id: userOneId, access: 'auth' }, process.env.JWT_SECRET).toString()
 							}]
 						}, {    // user without authentication token
 							_id: userTwoId,
@@ -24,7 +24,7 @@ const users         = [{    // user with authentication token
 							password: 'userTwoPass',
 							tokens: [{
 								access: 'auth',
-								token: jwt.sign({ _id: userTwoId, access: 'auth' }, 'abc123').toString()
+								token: jwt.sign({ _id: userTwoId, access: 'auth' }, process.env.JWT_SECRET).toString()
 							}]
 						}];
 
